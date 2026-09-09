86facb00e308d9370ccf38bd18b39ecbe0503303
deltaMethod-id <wahyudede930@gmail.com>
2026-09-09 21:03:53 +0700

    Initial commit

--- app/src/main/java/org/hosts/connection/BaseActivity.java
+++ app/src/main/java/org/hosts/connection/BaseActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
